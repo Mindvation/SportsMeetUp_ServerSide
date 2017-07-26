956901e1c9d76add51e4676e3da3201349712e01
xianboy <xianboy2@sina.com>
2017-07-26 15:55:54 +0800

    update pom file of papi

--- sports-meetup-user/src/main/java/com/newlife/meetup/config/SmsConfig.java
+++ sports-meetup-user/src/main/java/com/newlife/meetup/config/SmsConfig.java
@@ -1,4 +1,4 @@
-package com.newlife.meetup.domain;
+package com.newlife.meetup.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
